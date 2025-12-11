c83d0325423c88d46e1e02b62498fc87af844124
Lu69as <lukasokken@gmail.com>
2025-12-11 11:36:19 +0100

    Day 1, part 2

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const output = document.querySelector("#consoleOutput");
 
 
 /* 🔔 -------------------------------------- 🎄 */
-/* 🧑‍🎄 -> Day 1: Secret Entrance
+/* 🧑‍🎄 -> Day 1: Secret Entrance - Part 1
 /* 🎄 -------------------------------------- 🔔 */
-function secretEntrance() {
+function secretEntrancePart1() {
     let input = inputField.value
     let lockPos = 50;
     let passWord = 0;
@@ -20,6 +20,46 @@ function secretEntrance() {
         while (lockPos > 99) lockPos = lockPos - 100
         while (lockPos < 0) lockPos = 100 + lockPos;
         if (lockPos == 0) passWord++;
+    });
+
+    output.innerText = passWord;
+}
+
+
+/* 🔔 -------------------------------------- 🎄 */
+/* 🧑‍🎄 -> Day 1: Secret Entrance - Part 2
+/* 🎄 -------------------------------------- 🔔 */
+function rotateNum(num, input) {
+    let change = input.slice(1) - 0;
+    let dir = input.charAt()
+
+    if (dir == "L") num = num - change;
+    else if (dir == "R") num = num + change;
+    return(num);
+}
+
+function secretEntrancePart2() {
+    console.clear();
+    let input = inputField.value
+    let lockPos = 50, passWord = 0;
+    let changes = input.split("\n");
+
+    changes.forEach(c => {
+        let change = c.slice(1) - 0;
+        let NumUp = c.charAt() == "R";
+        
+        for (let i = 0; i < change; i++) {
+            if (NumUp) lockPos++;
+            else lockPos--;
+
+            if (lockPos > 99) {
+                lockPos = 0;
+                passWord++;
+            }
+            else if (lockPos < 0) lockPos = 99;
+            else if (lockPos == 0) passWord ++;
+        }
+        console.log(lockPos + " " + c)
     });
 
     output.innerText = passWord;
